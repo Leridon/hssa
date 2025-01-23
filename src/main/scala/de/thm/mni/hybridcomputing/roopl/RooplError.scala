ae8f69fceaa1936562c8f1466b07f8f2bbc34b64
Tristan Schönhals <tristan.schoenhals@mni.thm.de>
2025-01-23 19:14:56 +0100

    Add RooplError for Wellformedness checks
    Signed-off-by: Tristan Schönhals <tristan.schoenhals@mni.thm.de>

---
+++ src/main/scala/de/thm/mni/hybridcomputing/roopl/RooplError.scala
@@ -0,0 +1,11 @@
+package de.thm.mni.hybridcomputing.roopl
+
+import de.thm.mni.hybridcomputing.util.errors.LanguageError
+import de.thm.mni.hybridcomputing.util.parsing.SourcePosition
+
+class RooplError(severity: LanguageError.Severity,
+                message: String,
+                position: SourcePosition = null
+               ) extends LanguageError(severity, message, position) {
+    
+}
