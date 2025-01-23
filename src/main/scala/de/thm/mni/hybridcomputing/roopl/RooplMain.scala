9b0ab83f7df17254581f0aea4fc80bb925d42e33
Tristan Schönhals <tristan.schoenhals@mni.thm.de>
2025-01-23 13:15:44 +0100

    Add properly runnable main for roopl including usage and options
    Signed-off-by: Tristan Schönhals <tristan.schoenhals@mni.thm.de>

--- src/main/scala/de/thm/mni/hybridcomputing/roopl/RooplMain.scala
+++ src/main/scala/de/thm/mni/hybridcomputing/roopl/RooplMain.scala
@@ -8,30 +8,65 @@ import de.thm.mni.hybridcomputing.roopl.parsing.Lexing.Tokens.TokenClass
 import de.thm.mni.hybridcomputing.util.parsing.TokenReader
 import de.thm.mni.hybridcomputing.roopl.parsing.Parsing
 import de.thm.mni.hybridcomputing.util.errors.LanguageError
+import java.nio.file.NoSuchFileException
+import de.thm.mni.hybridcomputing.roopl.Syntax.Program
 
 object RooplMain {
   def main(args: Array[String]): Unit = {
-    val file = "programs/roopl/ParserTest.rplpp"
+    if args.size < 1 then
+        usage()
 
-    val tokenStream: TokenReader[TokenClass] = lex(SourceFile.fromFile(Paths.get(file)))
+    var showTokens: Boolean = false
+    var showFormat: Boolean = false
+    var file: String = ""
 
-    /*
-    val tokens = tokenStream.readAll()
+    for (arg <- args) {
+        arg match
+            case "--tokens" => showTokens = true 
+            case "--format" => showFormat = true
+            case string => if file == "" then file = string else usage()
+    }
 
-    tokens.foreach(println)
-    */
+    if (file == "") usage()
 
     try {
-      val prog = Parsing.parse(tokenStream)
-      val formatter = Formatting(new Formatting.Options(parenthesizeExpressions = false, indentBy = 4))
-      println("Success!")
-      println(formatter.format(prog))
+        val tokenStream: TokenReader[TokenClass] = lex(SourceFile.fromFile(Paths.get(file)))
+        if showTokens then
+            tokenStream.readAll().foreach(println)
+            sys.exit(0)
+        if showFormat then
+            val formatter = Formatting(new Formatting.Options(parenthesizeExpressions = false, indentBy = 4))
+            parse(tokenStream) match
+                case Some(prog) => println(formatter.format(prog))
+                case None => ;
+            sys.exit(0)
+        
+        // Run semantic analysis
+    } catch {
+        case e: NoSuchFileException =>
+            s"File '$file' does not exist!"
+    }
+  }
+
+  def parse(tokenStream: TokenReader[TokenClass]): Option[Program] = {
+    try {
+      Some(Parsing.parse(tokenStream))
     } catch {
         case e: LanguageError.AbortDueToErrors =>
             e.errors.foreach(e => {
                 println(s"An error occurred @ ${e.position}")
                 println(e.msg)
             })
-      }
+            None
+    }
+  }
+
+  def usage(): Unit = {
+    println("Usage: <RooplMain> [option]... file")
+    println("       --tokens")
+    println("           Print tokenstream, then exit")
+    println("       --format")
+    println("           Print formatted program, then exit")
+    sys.exit(1)
   }
 }
--- src/main/scala/de/thm/mni/hybridcomputing/roopl/TODO.md
+++ src/main/scala/de/thm/mni/hybridcomputing/roopl/TODO.md
@@ -1,14 +1,19 @@
 # ROOPL++ compiled to HSSA, TODOs:
+- [x] Done
+- [-] In progress
+- [ ] TBD
+
 
 - [x] Syntax
 - [x] Scanner
-- [-] Parser
+- [x] Parser
     - [x] Full implementation of Syntax
-    - [ ] Testing
+    - [x] Testing
 - [-] Formatting
     - [x] Print Syntax as SourceCode
     - [ ] Add newlines where sensible
     - [ ] Don't loose comments when formatting (requires comments in Syntax)
+- [-] CLI-usable main
 - [ ] Semantic Analysis
     - ...
 - [ ] Translation into HSSA
