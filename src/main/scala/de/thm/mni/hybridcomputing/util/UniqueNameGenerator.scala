7f245a79f20f27f65cd75f796f41a42bbe2ace14
Lukas Gail <lukas.gail@mni.thm.de>
2025-04-02 17:24:54 +0200

    Adds unique name generator with tests

---
+++ src/main/scala/de/thm/mni/hybridcomputing/util/UniqueNameGenerator.scala
@@ -0,0 +1,47 @@
+package de.thm.mni.hybridcomputing.util
+
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+
+class UniqueNameGenerator(separator: String = ".") {
+    private val reserved_functions = new ListBuffer[String => Boolean]()
+    private val reserved = mutable.Set[String]()
+    
+    def reserve(f: String => Boolean): Unit = this.reserved_functions.addOne(f)
+    
+    def isReserved(name: String): Boolean = this.reserved.contains(name) || reserved_functions.exists(f => f(name))
+    
+    def get(name: String): String = {
+        val split = UniqueNameGenerator.splitName(name, this.separator)
+        
+        val generated_name = LazyList.from(split.postfix.map(_ + 1).getOrElse(0))
+          .map(i => UniqueNameGenerator.SplitName(split.prefix, this.separator, Some(i)))
+          .find(n => !this.isReserved(n.combined))
+          .map(_.combined)
+          .get
+        
+        this.reserved.addOne(generated_name)
+        
+        generated_name
+    }
+}
+
+object UniqueNameGenerator {
+    case class SplitName(prefix: String, separator: String, postfix: Option[Int]) {
+        lazy val combined: String = postfix match
+            case Some(value) => prefix + separator + value
+            case None => prefix
+    }
+    
+    def splitName(name: String, separator: String = "."): SplitName = {
+        val version_string = name.reverse.takeWhile(_.isDigit).reverse
+        
+        if (version_string.isEmpty) return SplitName(name, separator, None)
+        
+        val without_version = name.dropRight(version_string.length)
+        
+        if (!without_version.endsWith(separator)) return SplitName(name, separator, None)
+        
+        SplitName(without_version.dropRight(separator.length), separator, Some(version_string.toInt))
+    }
+}
---
+++ src/test/scala/de/thm/mni/hybridcomputing/tests/UniqueNameGeneratorTests.scala
@@ -0,0 +1,23 @@
+package de.thm.mni.hybridcomputing.tests
+
+import de.thm.mni.hybridcomputing.util.UniqueNameGenerator
+import org.scalatest.flatspec.AnyFlatSpec
+
+class UniqueNameGeneratorTests extends AnyFlatSpec:
+    
+    "A UniqueNameGenerator" should "generate unique names" in {
+        val generator = new UniqueNameGenerator(".")
+        
+        assert(generator.get("x") == "x.0")
+        assert(generator.get("x") == "x.1")
+        assert(generator.get("x") == "x.2")
+    }
+    
+    it should "respect external reservations" in {
+        val generator = new UniqueNameGenerator(".")
+        
+        generator.reserve(_.endsWith("1"))
+        
+        assert(generator.get("x") == "x.0")
+        assert(generator.get("x") == "x.2")
+    }
