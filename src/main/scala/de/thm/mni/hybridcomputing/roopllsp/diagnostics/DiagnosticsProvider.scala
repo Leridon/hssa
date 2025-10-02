029bfd7275e36be5da431bb51d9ebffcff434e94
chht25 <christian.hecht@mni.thm.de>
2025-10-02 15:05:32 +0200

    Fixed position calculation

--- src/main/scala/de/thm/mni/hybridcomputing/roopllsp/diagnostics/DiagnosticsProvider.scala
+++ src/main/scala/de/thm/mni/hybridcomputing/roopllsp/diagnostics/DiagnosticsProvider.scala
@@ -8,7 +8,7 @@ import de.thm.mni.hybridcomputing.roopllsp.ROOPLTextDocumentService
 import de.thm.mni.hybridcomputing.util.errors.LanguageError
 import de.thm.mni.hybridcomputing.util.errors.LanguageError.Severity
 import de.thm.mni.hybridcomputing.util.errors.LanguageError.Severity.Warning
-import de.thm.mni.hybridcomputing.util.parsing.{SourceFile, TokenReader}
+import de.thm.mni.hybridcomputing.util.parsing.{SourceFile, SourcePosition, TokenReader}
 import org.eclipse.lsp4j.{Diagnostic, DiagnosticSeverity, DocumentDiagnosticReport, Position, RelatedFullDocumentDiagnosticReport}
 
 class DiagnosticsProvider {
@@ -19,16 +19,17 @@ class DiagnosticsProvider {
       diagnostics = new java.util.ArrayList[Diagnostic]()
       val text = documentService.getOpenFiles.get(uri)
       if (text.isDefined) {
+        val sourceFile = SourceFile.fromString(text.get)
         try {
-          val tokenStream: TokenReader[TokenClass] = Lexing.lex(SourceFile.fromString(text.get))
-          tokenStream.readAll().foreach(token => println(s"$token @ ${token.position.toString}"))
+          val tokenStream: TokenReader[TokenClass] = Lexing.lex(sourceFile)
+          //tokenStream.readAll().foreach(token => println(s"$token @ ${token.position.toString}"))
           val syntax: Program = Parsing.parse(tokenStream)
           val graph: ClassGraph.Program = ClassGraph.check(syntax)
           val scopes: ScopeTree.Program = Wellformedness.check(graph)
         }
         catch {
           case e: LanguageError.AbortDueToErrors =>
-            e.errors.foreach(e => buildDiagnostic(e))
+            e.errors.foreach(e => buildDiagnostic(e, sourceFile))
           case e: NullPointerException =>
             e.printStackTrace()
         }
@@ -37,18 +38,45 @@ class DiagnosticsProvider {
       DocumentDiagnosticReport(related)
   }
   
-  private def buildDiagnostic(error: LanguageError) : Unit = {
-      var range = org.eclipse.lsp4j.Range(Position(0, 0), Position(0, 0))
-      if (error.position.to != null && error.position.from != null) {
-        range = org.eclipse.lsp4j.Range(Position(error.position.from.line, error.position.from.column),
-          Position(error.position.to.line, error.position.to.column))
+  private def buildDiagnostic(error: LanguageError, sourceFile: SourceFile) : Unit = {
+    val errorRange = adjustErrorRange(error, sourceFile)
+    val range = org.eclipse.lsp4j.Range(errorRange._1, errorRange._2)
+    val message = error.msg
+    val severity = error.severity match {
+      case Severity.Error => DiagnosticSeverity.Error
+      case Severity.Warning => DiagnosticSeverity.Warning
+    }
+    val source = "ROOPL LSP"
+    diagnostics.add(Diagnostic(range, message, severity, source))
+  }
+  
+  private def adjustErrorRange(error: LanguageError, sourceFile: SourceFile): (Position, Position) = {
+    var fromLine: Int = 1
+    var fromColumn: Int = 1
+    var toLine: Int = 1
+    var toColumn: Int = 1
+    if (error.position != null)
+      if (error.position.from != null) {
+        fromLine = error.position.from.line
+        fromColumn = error.position.from.column
       }
-      val message = error.msg
-      val severity = error.severity match {
-        case Severity.Error => DiagnosticSeverity.Error
-        case Severity.Warning => DiagnosticSeverity.Warning
+      if (error.position.to != null) {
+        toLine = error.position.to.line
+        toColumn = error.position.to.column
       }
-      val source = "ROOPL LSP"
-      diagnostics.add(Diagnostic(range, message, severity, source))
+      else {
+        toLine = fromLine
+        toColumn = fromColumn
+      }
+    else println("SERVER: Error " + error.msg + " has no position")
+    
+    val lineCount = sourceFile.getSlice(SourcePosition.Position(fromLine, fromColumn), 
+      SourcePosition.Position(toLine, toColumn)).trim.lines().count().toInt
+    if (lineCount <= 1) {
+      toLine = fromLine
+      toColumn = sourceFile.getLine(fromLine).length
+    }
+    else toLine = fromLine + lineCount
+    (Position(fromLine - 1, fromColumn - 1), Position(toLine - 1, toColumn - 1))
   }
 }
